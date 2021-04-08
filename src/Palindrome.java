805591859d66991f7230955be87126feb774b464
VorobevS <81978047+VorobevS@users.noreply.github.com>
2021-04-09 01:38:43 +0300

    Update Palindrome.java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Palindrome {
+import java.util.Scanner;
+public class Palindrome {
+    //Ввод текста из командной строки
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];}
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Введите текст: ");
+            String s=scan.next();
+            isPalindrome(s);
+
+    }
+    //Метод изменения символов в строке
+    public static String reverseString(String s){
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; --i)
+            r += s.charAt(i);
+        return r;
+
+
+    }
+    //Проверка на полидром
+    public static Boolean isPalindrome(String s) {
+        if(s.equals(reverseString(s))){
+            System.out.println("Полиндром");
+        }else{
+            System.out.println("Не полиндром");
+        }
+        return s.equals(reverseString(s));
+
+    }
 }
